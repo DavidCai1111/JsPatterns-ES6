9e0cc83682ed5c6e49e75f3e5adba5cf14a549c9
DavidCai <376462191@qq.com>
2015-05-27 18:53:51 +0800

    sub_pub

---
+++ sub_pub.js
@@ -0,0 +1,22 @@
+class Event {
+  constructor() {
+    this.subscribers = new Map([['any', []]]);
+  }
+
+  on(fn, type = 'any') {
+    let subs = this.subscribers;
+    if (!subs.get(type)) return subs.set(type, [fn]);
+    subs.set(type, (subs.get(type).push(fn)));
+  }
+
+  emit(content, type = 'any') {
+    for (let fn of this.subscribers.get(type)) {
+      fn(content);
+    }
+  }
+}
+
+let event = new Event();
+
+event.on((content) => console.log(`get published content: ${content}`), 'myEvent');
+event.emit('jaja', 'myEvent');
